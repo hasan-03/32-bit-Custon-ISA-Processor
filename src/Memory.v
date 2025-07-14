c225cf5cc68749116439fb74f13c38c2c5bd6c75
Hasan Ali <143880255+hasan-03@users.noreply.github.com>
2025-07-14 21:24:45 +0530

    Update Memory.v

--- src/Memory.v
+++ src/Memory.v
@@ -24,22 +24,41 @@ module memory (
             mem[i] = 32'b0;
         
         // Instruction
-        // Sample example 
-      mem[0] = 32'h02001400;  // load M[20]->R0
-      mem[1] = 32'h02001501;  // load M[21]->R1
-      mem[2] = 32'h05000000;  // Skip next instruction if R0>0
-      mem[3] = 32'h0B020100;  // AND(R0,R1)->R2
-      mem[4] = 32'h01001902;  // Store M[19]<-R2
-      mem[5] = 32'h24000000;  // HLT
+        // Sample example
+        
+        // ADD operation
+        
+//        mem[0] = 32'h02001400;  // load M[20]->R0
+//        mem[1] = 32'h02001501;  // load M[21]->R1
+//        mem[3] = 32'h07020100;  // ADD(R0,R1)->R2
+//        mem[4] = 32'h01001902;  // Store M[19]<-R2
+//        mem[5] = 32'h24000000;  // HLT
+    
+        
+        // Conditional AND operation
+        
+//      mem[0] = 32'h02001400;  // load M[20]->R0
+//      mem[1] = 32'h02001501;  // load M[21]->R1
+//      mem[2] = 32'h04000000;  // Skip next instruction if R0>0
+//      mem[3] = 32'h0B020100;  // AND(R0,R1)->R2
+//      mem[4] = 32'h01001902;  // Store M[19]<-R2
+//      mem[5] = 32'h24000000;  // HLT
+        
+        // For loop
+        
+//        mem[0] = 32'h02001400;  // load M[20]->R0
+//        mem[1] = 32'h17000000;  // INC R0
+//        mem[2] = 32'h06001000;  // If R0 == IR[23:8] skip next instrction
+//        mem[3] = 32'h03000100;  // pc=16'h0001
+//        mem[4] = 32'h01001900;  // Store M[19]<-R0
+//        mem[5] = 32'h24000000;  // HLT
         
         // Data section
-        mem[20] = 32'h00000010; 
+        mem[20] = 32'h0000000D; 
         mem[21] = 32'h00000011; 
         mem[22] = 32'h00000001; 
         mem[23] = 32'h00000002;
         mem[24] = 32'h00000005; 
     end
 
-    
 endmodule
-
